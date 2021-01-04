781b9840428ade925f97e7f02895106805c0da8e
inggolanda <inggolandaa@gmail.com>
2021-01-04 10:16:15 +0700

    CV Portfolio Project

---
+++ resources/views/cv/index.blade.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">
+
+    <!--==== SWIPER CSS======-->
+    <link rel="stylesheet" href="{{asset('assets/css/swiper-bundle.min.css')}}">
+
+
+    <!--==== BOX ICONS======-->
+    <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel='stylesheet'>
+
+    <!--====CSS======-->
+    <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
+
+    <title>Portfolio CV</title>
+</head>
+
+<body>
+    <!--==== SCROLL TOP====--->
+    <a href="" class="scrolltop" id="scroll-top">
+        <i class="bx bx-chevron-up scrolltop__icon"></i>
+    </a>
+
+    <!--====HEADER====--->
+    <header class="l-header" id="header">
+        <nav class="nav bd-container">
+            <a href="#" class="nav_logo">Inggo Landa</a>
+
+            <div class="nav_menu" id="nav-menu">
+                <ul class="nav_list">
+                    <li class="nav_item"><a href="#home" class="nav_link active-link">Home</a></li>
+                    <li class="nav_item"><a href="#about" class="nav_link">About</a></li>
+                    <li class="nav_item"><a href="#sevices" class="nav_link">Skills</a></li>
+                    <li class="nav_item"><a href="#portfolio" class="nav_link">Portfolio</a></li>
+                    <li class="nav_item"><a href="#contact" class="nav_link">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="nav_toggle" id="nav-toggle">
+                <i class="bx bx-menu"></i>
+            </div>
+        </nav>
+    </header>
+
+
+    <main class="l-main">
+        <!--====HOME====--->
+        <section class="home" id="home">
+            <div class="home__container bd-container bd-grid">
+                <div class="home__data">
+                    <span class="home__greeting">Hello, My Name Is</span>
+                    <h1 class="home_name">Inggo Landa</h1>
+                    <span class="home__profession">Web Developer</span>
+                    <a download="" href="#" class="button button-light home__button">Download</a>
+                </div>
+
+                <div class="home__social">
+                    <a href="" class="home__social-icon"><i class="bx bxl-facebook-square"></i></a>
+                    <a href="" class="home__social-icon"><i class="bx bxl-instagram"></i></a>
+                    <a href="" class="home__social-icon"><i class="bx bxl-whatsapp"></i></a>
+                </div>
+
+                <div class="home__img">
+                    <img src="assets/img/Home.png" alt="">
+                </div>
+            </div>
+        </section>
+        <!--====ABOUT====--->
+        <section class="about section bd-container" id="about">
+            <span class="section-subtitle">My History</span>
+            <h2 class="section-title">About me</h2>
+
+            <div class="about__container bd-grid">
+                <div class="about__data bd-grid">
+                    <p class="aboute__description"><span>Hello I am<br>graduated with a Bachelor Technical Information of from the Indraprasta PGRI University, I am looking for company that can increase my potential. I am very interested in working of Software Development, IT Support , and Marketing. I like to learn something new and solve every challenge. Besides, I am a visionary person and I able work as individual and team. My principle is to do the best for every task</span></p>
+
+                </div>
+                <img src="assets/img/aboutme.jpg" alt="" class="about__img">
+            </div>
+        </section>
+
+        <!--====QUALIFICATION====--->
+        <section class="qualification section bd-container">
+            <span class="section-subtitle">Experience and education</span>
+            <h2 class="section-title">Qualification</h2>
+
+            <div class="qualification__container bd-grid">
+                <div class="qualification__content">
+                    <h2 class="qualification__title">
+                        <i class="bx bx-briefcase-alt qualification__title-icon"></i>
+                        Work Experience
+                    </h2>
+
+                    <div class="bd-grid">
+                        <div class="qualification__data">
+                            <h3 class="qualification__area">Cashier</h3>
+
+                            <div class="qualification__box">
+                                <span class="qualification__work">
+                                    <i class="bx bx-briefcase-alt qualification__icon"></i>
+                                    PT. Securindo Packatama Indonesia - Jakarta
+                                </span>
+
+                                <span class="qualification__work">
+                                    <i class="bx bx-calendar-alt qualification__icon"></i>
+                                    Dec 2015 - Nov 2017
+                                </span>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="qualification__data">
+                        <h3 class="qualification__area">Junior 3D Animator</h3>
+
+                        <div class="qualification__box">
+                            <span class="qualification__work">
+                                <i class="bx bx-briefcase-alt qualification__icon"></i>
+                                3Dgoon Studio - Bogor
+                            </span>
+
+                            <span class="qualification__work">
+                                <i class="bx bx-calendar-alt qualification__icon"></i>
+                                Dec 2017 - mart 2018
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="qualification__data">
+                        <h3 class="qualification__area">INTERNSHIP ACCOUNTING</h3>
+
+                        <div class="qualification__box">
+                            <span class="qualification__work">
+                                <i class="bx bx-briefcase-alt qualification__icon"></i>
+                                PT Citra Pangan Indah - Cibinong
+                            </span>
+
+                            <span class="qualification__work">
+                                <i class="bx bx-calendar-alt qualification__icon"></i>
+                                Oct 2018 - Mart 2019
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="qualification__data">
+                        <h3 class="qualification__area">Public Relations and Marketing</h3>
+
+                        <div class="qualification__box">
+                            <span class="qualification__work">
+                                <i class="bx bx-briefcase-alt qualification__icon"></i>
+                                PT Pelatos Nasional Indonesia - Depok
+                            </span>
+
+                            <span class="qualification__work">
+                                <i class="bx bx-calendar-alt qualification__icon"></i>
+                                Nov 2017 - Currently
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="qualification__content">
+                    <h2 class="qualification__title">
+                        <i class="bx bx-bookmark-alt qualification__title-icon"></i>
+                        Education
+                    </h2>
+
+                    <div class="bd-grid">
+                        <div class="qualification__data">
+                            <h3 class="qualification__area">Technical Information</h3>
+
+                            <div class="qualification__box">
+                                <span class="qualification__work">
+                                    <i class="bx bx-book-alt qualification__icon"></i>
+                                    University Indraprasta PGRI0 - Jakarta
+                                </span>
+
+                                <span class="qualification__work">
+                                    <i class="bx bx-calendar-alt qualification__icon"></i>
+                                    Nov 2016 - Agust 2020
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="qualification__data">
+                            <h3 class="qualification__area">Data Scintist</h3>
+
+                            <div class="qualification__box">
+                                <span class="qualification__work">
+                                    <i class="bx bx-book-alt qualification__icon"></i>
+                                    Online - Skill Academi
+                                </span>
+
+                                <span class="qualification__work">
+                                    <i class="bx bx-calendar-alt qualification__icon"></i>
+                                    -
+                                </span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--====SERVICES====--->
+        <section class="service section bd-container" id="sevices">
+            <span class="section-subtitle">What I Offer</span>
+            <h2 class="section-title">Skills</h2>
+
+            <div class="services__container bd-grid">
+                <div class="services__data">
+                    <i class="bx bx-laptop services__icon"></i>
+                    <h3 class="services__title">Web development</h3>
+                    <p class="services__description">Front end and back end</p>
+                    <a href="#" class="button">Know More</a>
+                </div>
+
+                <div class="services__data">
+                    <i class="bx bx-palette services__icon"></i>
+                    <h3 class="services__title">Editor</h3>
+                    <p class="services__description">Editor use Vegas for content</p>
+                    <a href="#" class="button">Know More</a>
+                </div>
+
+                <div class="services__data">
+                    <i class="bx bx-palette services__icon"></i>
+                    <h3 class="services__title">Marketing</h3>
+                    <p class="services__description">Marketing Comunicat</p>
+                    <a href="#" class="button">Know More</a>
+                </div>
+            </div>
+
+        </section>
+
+        <!--====PROJECT IN MIND====--->
+        <section class="project section bd-container">
+            <div class="project__container bd-grid">
+                <div class="project__data">
+                    <i class="bx bx-chat project__icon"></i>
+
+                    <div>
+                        <h2 class="project__title">Project In Mind</h2>
+                        <p class="project__description">I have plan make Program For Accounting</p>
+                    </div>
+
+                    <div>
+                        <a href="" class="button button-white">Hire me</a>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <!--====PORTFOLIO====--->
+        <section class="portfolio section bd-container" id="portfolio">
+            <span class="section-subtitle">My Recent Works</span>
+            <h2 class="section-title">Portfolio</h2>
+
+            <div class="portfolio__nav">
+                <span class="portfolio__item active-portfolio" data-filter="all">All</span>
+                <span class="portfolio__item" data-filter=".Native">Native</span>
+                <span class="portfolio__item" data-filter=".Laravel">Laravel</span>
+                <span class="portfolio__item" data-filter=".Angular">Angular</span>
+                <span class="portfolio__item" data-filter=".Java">Java</span>
+            </div>
+
+            <div class="portfolio__container bd-grid">
+                <div class="portfolio__content mix Native">
+                    <a href=""><img src="assets/img/work1.jpg" alt="" class="portfolio__img"></a>
+                    <div class="portfolio__data">
+                        <span class="portfolio__subtitle">Rapot Sekolah</span>
+                        <a href="">
+                            <h2 class="portfolio__title">New Portfolio</h2>
+                        </a>
+                        <a href="" class="button button-link">View Details</a>
+                    </div>
+                </div>
+
+                <div class="portfolio__content mix Laravel">
+                    <a href=""><img src="assets/img/work2.jpg" alt="" class="portfolio__img"></a>
+                    <div class="portfolio__data">
+                        <span class="portfolio__subtitle">Cv-Portfolio</span>
+                        <a href="">
+                            <h2 class="portfolio__title">New Portfolio</h2>
+                        </a>
+                        <a href="" class="button button-link">View Details</a>
+                    </div>
+                </div>
+
+                <div class="portfolio__content mix Angular">
+                    <a href=""><img src="assets/img/work3.jpg" alt="" class="portfolio__img"></a>
+                    <div class="portfolio__data">
+                        <span class="portfolio__subtitle">Pesananan Film</span>
+                        <a href="">
+                            <h2 class="portfolio__title">New Portfolio</h2>
+                        </a>
+                        <a href="" class="button button-link">View Details</a>
+                    </div>
+                </div>
+
+                <div class="portfolio__content mix Java">
+                    <a href=""><img src="assets/img/work4.jpeg" alt="" class="portfolio__img"></a>
+                    <div class="portfolio__data">
+                        <span class="portfolio__subtitle">Email Blast</span>
+                        <a href="">
+                            <h2 class="portfolio__title">New Portfolio</h2>
+                        </a>
+                        <a href="" class="button button-link">View Details</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--====Contact====--->
+        <section class="contact section bd-container" id="contact">
+            <span class="section-subtitle">For Projects</span>
+            <h2 class="section-title">Contact Me</h2>
+
+            <div class="contact__container bd-grid">
+                <div class="contact__container bd-grid">
+                    <div class="contact__box">
+                        <i class="bx bx-home contact__icon"></i>
+                        <h3 class="contact__title">Location</h3>
+                        <span class="contact__description">Bogor - Indonesia</span>
+                    </div>
+                </div>
+
+                <div class="contact__box">
+                    <i class="bx bx-phone contact__icon"></i>
+                    <h3 class="contact__title">Phone</h3>
+                    <span class="contact__description">083811536395</span>
+                </div>
+            </div>
+            </div>
+        </section>
+
+    </main>
+    <!--====FOOTER====--->
+    <footer class="footer">
+        <div class="footer__description bd-container">
+            <h1 class="footer__title">Inggo Landa</h1>
+            <p class="footer__description"></p>
+
+            <div class="footer__social">
+                <a href="" class="footer__link"><i class="bx bxl-linkedin"></i></a>
+                <a href="" class="footer__link"><i class="bx bxl-github"></i></a>
+            </div>
+            <p class="footer__copy">&#169; Inggo Landa</p>
+        </div>
+    </footer>
+    <!--====MIXITUP FILTER====--->
+    <script src="{{asset('assets/js/mixitup.min.js')}}"></script>
+
+    <!--====GSAP====--->
+    <script src="{{asset('assets/js/gsap.min.js')}}"></script>
+
+    <!--====Swiper====--->
+    <script src="{{asset('assets/js/swiper-bundle.min.js')}}"></script>
+
+    <!--====Main JS====--->
+    <script src="{{asset('assets/js/main.js')}}"></script>
+
+
+
+</body>
+
+</html>
